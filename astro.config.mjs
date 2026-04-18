0070f5cb9d51db2fb86133abd78d9a9a5b437417
francisdeea <francisbernal14@gmail.com>
2026-04-18 21:02:19 +0200

    chore: fix react hydration issues in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'react-dom/client'],
+    },
   },
 
   i18n: {
